f0a5bb7757625c2ba6f8ad346c5beeedd86b0ba6
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-07-22 14:18:43 +0100

    starting to add the ingredients

--- public/app.js
+++ public/app.js
@@ -1,3 +1,9 @@
+var state = {
+  beers: [],
+  beer: 0
+}
+
+
 window.onload = function() {
   console.log( 'app started' )
   var url = "https://punkapi.com/api/v1/beers"
@@ -9,9 +15,8 @@ window.onload = function() {
   request.onload = function() {
     if (request.status === 200) {
       var jsonString = request.responseText;
-      var beers = JSON.parse(jsonString);
-      console.log(beers);
-      main( beers )
+      state.beers = JSON.parse(jsonString);
+      main()
     }
   }
 
@@ -19,10 +24,11 @@ window.onload = function() {
 
 }
 
-var main = function( beers ) {
+var main = function() {
 
   var beerSelect = document.getElementById( "beerSelect" );
-  beers.forEach( function( beer, index ) {
+  var recipeSelect = document.getElementById( "recipe-button" )
+  state.beers.forEach( function( beer, index ) {
     var option = document.createElement( 'option' );
     option.innerText = beer.name;
     option.value = index;
@@ -31,7 +37,45 @@ var main = function( beers ) {
   }) 
 
   beerSelect.onchange = function( event ) {
-    console.log( event.target.value )
+    var index = event.target.value 
+    state.beer = state.beers[index]
+    updateDisplay( state.beer )
+  }
+
+  recipeSelect.onclick = function( beer, event ) {
+    displayRecipe( state.beer )
   }
 
 }
+
+var updateDisplay = function ( beer ) {
+  var tags = document.querySelectorAll('#info p');
+  tags[0].innerText = "Blurb: " + beer.tagline;
+  tags[1].innerText = "Description: " + beer.description;
+  tags[2].innerText = "ABV: " + beer.abv + "%";
+  console.log( beer )        
+}
+
+var displayRecipe = function( beer ) {
+  var ingredients = document.getElementById('recipe');
+
+
+
+
+  state.beer.ingredients.hops.forEach( function(hop, index) {
+    var p = document.createElement( 'p' )
+    p.id = "hops"
+    p.innerHTML = hop.name + ": "+ hop.amount.value + "g" + "<br>" + "Add at: " + hop.add;
+    ingredients.appendChild(p)
+  });
+
+
+}
+
+
+
+
+
+
+
+
